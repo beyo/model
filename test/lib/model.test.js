16d7b6652026bfc7e9642ad25ca44342a590e561
yanickrochon <yanick.rochon@gmail.com>
2014-08-26 15:52:02 -0400

    Added JSON alias support for module attributes. Used when creating model from JSON or calling toJSON

--- test/lib/model.test.js
+++ test/lib/model.test.js
@@ -36,7 +36,7 @@ describe('Test Model', function () {
     };
     var Foo2 = Model.define('Foo2', options);
 
-    var foo2 = new Foo2();
+    var foo2 = Foo2();
 
     foo2.should.be.instanceof(EventEmitter);
 
@@ -54,10 +54,14 @@ describe('Test Model', function () {
       }
     };
     var Foo3 = Model.define('Foo3', options);
+    var foo3;
 
-    var foo3 = new Foo3({ id: 123 });
-
+    foo3 = Foo3({ id: 123 });
     foo3.id.should.equal(123);
+
+    foo3 = Foo3({ id: 456, login: 'foo' });
+    foo3.id.should.equal(456);
+    foo3.login.should.equal('foo');
   });
 
   it('should fail to define invalid models', function () {
@@ -85,7 +89,6 @@ describe('Test Model', function () {
       +function () { Model.isDefined(modelType); }.should.throw();
       +function () { Model.get(modelType); }.should.throw();
     });
-
   });
 
   it('should allow custom getter / setter', function () {
@@ -102,7 +105,7 @@ describe('Test Model', function () {
     var TestGetter = Model.define('testGetter', options);
     var optionsValue = {'a': 1, 'b': 2};
 
-    var m = new TestGetter({ options: optionsValue });
+    var m = TestGetter({ options: optionsValue });
 
     m._data['options'].should.be.a.String.and.equal(JSON.stringify(optionsValue));
     m.options.should.not.equal(optionsValue);
@@ -138,11 +141,14 @@ describe('Test Model', function () {
         }
       }
     });
+    var model;
 
-    var model = Model.get(typeNameA)();
-
+    model = Model.get(typeNameA)();
+    model.b.c.foo.should.equal('foo');
     model.toJson().should.eql({ b: { c: { foo: 'foo' } } });
+
     model = model.fromJson({ b: { c: { foo: 'bar' } } });
+    model.b.c.foo.should.equal('bar');
     model.toJson().should.eql({ b: { c: { foo: 'bar' } } });
 
   });
@@ -159,7 +165,7 @@ describe('Test Model', function () {
       }
     });
 
-    var model = new Type({
+    var model = Type({
       id: 123,
       foo: 'Hello'
     });
@@ -184,7 +190,7 @@ describe('Test Model', function () {
   });
 
 
-  describe('instance methods', function () {
+  describe('Testing instance methods', function () {
 
     it('should define model methods', function () {
       var Type = Model.define('TestDefineMethods', {
@@ -205,7 +211,7 @@ describe('Test Model', function () {
   });
 
 
-  describe('static methods', function () {
+  describe('Testing static methods', function () {
 
     it('should define static methods', function () {
       var Type = Model.define('TestDefineStaticMethods', {
@@ -222,6 +228,82 @@ describe('Test Model', function () {
 
       Type.foo().should.equal('bar');
 
+    });
+
+  });
+
+  describe('Testing mapping', function () {
+
+    it('should set from JSON with mapping', function () {
+      var Type = Model.define('TestMappingFromJson', {
+        attributes: {
+          id: { type: 'text' },
+          firstName: { type: 'string', alias: 'first_name' },
+          lastName: { type: 'string', alias: 'last_name' },
+          foo: { type: 'integer', alias: 'bar' }
+        }
+      });
+
+      var model = Type({
+        id: 'abc',
+        first_name: 'John',
+        last_name: 'Doe',
+        foo: 123        // should still map, even if using the actual attribute name
+      });
+
+      model.id.should.equal('abc');
+      model.firstName.should.equal('John');
+      model.lastName.should.equal('Doe');
+      model.foo.should.equal(123);
+
+    });
+
+
+    it('should convert to JSON with mapping', function () {
+      var Type = Model.define('TestMappingToJson', {
+        attributes: {
+          id: { type: 'text', default: '123' },
+          firstName: { type: 'string', alias: 'first_name', default: 'John' },
+          lastName: { type: 'string', alias: 'last_name', default: 'Smith' },
+          foo: { type: 'integer', alias: 'bar' }
+        }
+      });
+
+      var model = Type();
+
+      model.toJson().should.eql({ id: '123', 'first_name': 'John', 'last_name': 'Smith' });
+    });
+
+  });
+
+
+  describe('Testing arrays', function () {
+
+    it('should create model from array', function () {
+      var Type = Model.define('TestArrayFromJson', {
+        attributes: {
+          values: {
+            type: 'integer[]'
+          },
+          inline: 'string[]',
+          objectList: {
+            type: 'object[]',
+            alias: 'object_list'
+          }
+        }
+      });
+
+      var model = Type({
+        values: [ 1, 2, 3 ],
+        inline: [ 'a', 'b', 'c' ],
+        objectList: [{}, {}, {}]
+      });
+
+      model.values.should.eql([1, 2, 3]);
+      model.inline.should.eql(['a','b','c']);
+      model.objectList.should.eql([{},{},{}]);
+
+      model.toJson().should.eql({values:[1, 2, 3], inline:['a','b','c'], object_list:[{},{},{}]});
     });
 
   });
