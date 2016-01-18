c59e0dd8a261cd8f138fd76a8a64d0eaf3c9d43a
Yanick Rochon <yanick.rochon@gmail.com>
2016-01-18 07:59:09 -0500

    Fixed misplaced test files. Added Model.undefine function for consistency.

--- test/model.test.js
+++ test/model.test.js
@@ -15,17 +15,99 @@ describe('Testing Model', function () {
       (function () { new TestModel(); }).should.throw('Undefined Model instance');
     });
 
-    it('should be instanceof Model');
+    it('should be instanceof Model', function () {
+
+      class TestModel_0 extends Model {}
+
+      Model.define(TestModel_0);
+
+      let t = new TestModel_0();
+
+      t.should.instanceOf(TestModel_0);
+      t.should.instanceOf(Model);
+    });
 
     it('should allow models with no defined attributes', function () {
 
-      class TestModel extends Model {}
+      class TestModel_1 extends Model {}
 
-      Model.define(TestModel);
+      Model.define(TestModel_1);
 
-      let t = new TestModel();
+      let t = new TestModel_1();
 
       t.toJson().should.deepEqual({});
+    });
+
+    it('should create from attributes', function () {
+
+      class TestModel_2 extends Model {}
+
+      Model.define(TestModel_2, {
+        foo: {
+          default: 'Foo'
+        }
+      });
+
+      let t = new TestModel_2();
+
+      t.foo.should.equal('Foo');
+      t.toJson().should.deepEqual({ foo: 'Foo' });
+    });
+
+    it('should fail with wrong model', function () {
+      [
+        undefined, null, false, true,
+        NaN, 0, 1, Infinity, '', 'foo',
+        function () {}, {}, [], /./, new Date()
+      ].forEach(function (model) {
+        (function () { Model.define(model); }).should.throw('Invalid model');
+        (function () { Model.define('InvalidModel', model); }).should.throw('Invalid model');
+        (function () { Model.define('InvalidModel', model, {}); }).should.throw('Invalid model');
+      });
+    });
+
+  });
+
+
+  describe('Getting models', function () {
+
+    it('should get defined models', function () {
+
+      class TestModel_3 extends Model {}
+
+      Model.isDefined(TestModel_3.name).should.be.false();
+      Model.define(TestModel_3);
+
+      Model.isDefined(TestModel_3.name).should.be.true();
+      Model.get(TestModel_3.name).should.equal(TestModel_3);
+    });
+
+    it('should get named defined models', function () {
+      const modelName = 'NamedModel_4';
+
+      class TestModel_4 extends Model {}
+
+      Model.define(modelName, TestModel_4);
+
+      Model.get(modelName).should.equal(TestModel_4);
+    });
+
+    it('should fail with invalid model name', function () {
+      [
+        undefined, null, false, true,
+        NaN, 0, 1, '',
+        function () {}, {}, [], /./, new Date()
+      ].forEach(function (name) {
+        (function () { Model.get(name); }).should.throw('Invalid model name');
+      });
+    });
+
+    it('should fail on unknown model', function () {
+      [
+        'InvalidModel'
+      ].forEach(function (name) {
+        (function () { Model.get(name); }).should.throw('Unknown model name');
+      });
     });
 
   });
---
+++ test/types/object.test.js
@@ -0,0 +1,12 @@
+/**
+Object validation
+*/
+'use strict';
+
+describe('Test Type Object', function () {
+
+  it('should validate true');
+
+  it('should throw');
+
+});
